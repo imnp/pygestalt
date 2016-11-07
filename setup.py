04f2ab9dc812538fcc7d385550ef5f22a6ebbe96
Ilan Moyer <imoyer@mit.edu>
2016-11-07 12:08:51 -0800

    added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pygestalt',
+      version='0.7',
+      description='Gestalt Machine Control Framework',
+      author='Ilan Moyer',
+      author_email='imoyer@mit.edu',
+      url='https://github.com/imoyer/pygestalt',
+      packages=['pygestalt'],
+      package_dir={'pygestalt':'source/.'}
+     )
