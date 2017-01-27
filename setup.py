487c94b823c8346013cd17ff86d512b96935ba38
Ilan Moyer <imoyer@mit.edu>
2017-01-27 03:24:33 -0800

    packaged pygestalt 0.7.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pygestalt',
-      version='0.7',
+      version='0.7.1',
       description='Gestalt Machine Control Framework',
       author='Ilan Moyer',
       author_email='imoyer@mit.edu',
